30c359a2c54a413d6f90a40f0e1729334ea9eba7
曾偉亮 <nick0904@hotmail.com>
2016-06-02 14:42:13 +0800

    Initial Commit

---
+++ CompareApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CompareApp
+//
+//  Created by 曾偉亮 on 2016/6/2.
+//  Copyright © 2016年 TSENG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
